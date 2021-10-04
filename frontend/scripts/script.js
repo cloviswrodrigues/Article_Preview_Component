29668907b1e9cc6648fbced72362c6b4b9bafb5e
cloviswrodrigues <cloviswrodrigues@gmail.com>
2021-10-04 00:45:47 -0300

    adicionado funcionalidade de mostrar as redes socias de compartilhamento ao clicar no botao share

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,8 +1,13 @@
 const icon = document.getElementById('icon_share');
-const share = 
 
 icon.addEventListener("click", function(){
-    
+    let cardProfile = document.getElementById("card_profile");
+    let share = document.getElementById("share");
+    let iconshare = document.getElementById("icon_share");
+
+    cardProfile.classList.toggle("none");
+    share.classList.toggle("none");
+    iconshare.classList.toggle("active");
 });
 
 console.log(icon);
